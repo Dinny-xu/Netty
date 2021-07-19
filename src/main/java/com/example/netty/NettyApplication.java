0d547a0d4e7179656ee89985a519aa8e1422c77a
dinny-xu <17501606708@163.com>
2021-07-19 09:39:08 +0800

    refactor: Netty框架搭建

---
+++ src/main/java/com/example/netty/NettyApplication.java
@@ -0,0 +1,30 @@
+package com.example.netty;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@Slf4j
+public class NettyApplication {
+
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(NettyApplication.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("spring.port");
+        String path = env.getProperty("spring.servlet.context-path");
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application Poney Gzxf Mobile is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port + path + "\n\t" +
+                "External: \thttp://" + ip + ":" + port + path + "\n\t" +
+                "swagger-ui: \thttp://" + ip + ":" + port + path + "swagger-ui.html\n\t" +
+                "Doc: \t\thttp://" + ip + ":" + port + path + "doc.html\n" +
+                "----------------------------------------------------------");
+    }
+}
